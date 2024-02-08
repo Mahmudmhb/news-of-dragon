213a14c3e1f59396fad159906a5bb44aabb246c0
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-02-08 10:22:50 +0600

    install firebase router tailwind daisyui

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaSNnRDJdKA13DpY0ob-p4qo6LvNwyzfk",
+  authDomain: "news-of-dragon-81309.firebaseapp.com",
+  projectId: "news-of-dragon-81309",
+  storageBucket: "news-of-dragon-81309.appspot.com",
+  messagingSenderId: "1072640299488",
+  appId: "1:1072640299488:web:12023f32f755e901165b85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
